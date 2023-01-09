2e8040e3922b14fe8503101c12498f0b45ba8ea8
wamun <yves@debyoo.com>
2023-01-10 00:20:18 +0100

    Update SystemMessage.tsx

--- src/SystemMessage/SystemMessage.tsx
+++ src/SystemMessage/SystemMessage.tsx
@@ -7,9 +7,30 @@ import { ISystemMessageProps } from '../type'
 const SystemMessage: React.FC<ISystemMessageProps> = props => {
   return (
     <div className={classNames('rce-container-smsg', props.className)}>
-      <div className='rce-smsg'>
-        <div className='rce-smsg-text'>{props.text}</div>
-      </div>
+      {(props.text === 'closed_this_case' || props.text === 'auto_closed_this_case') &&
+        <div className='rce-smsg-red'>
+          <div className='rce-smsg-text'>{props.text}</div>
+        </div>    
+      }
+      {(props.text === 'opened_a_case' || props.text === 'reopened_this_case') &&
+        <div className='rce-smsg-green'>
+          <div className='rce-smsg-text'>{props.text}</div>
+        </div>    
+      }
+      {props.text === 'connected_to_serve_you' &&
+        <div className='rce-smsg-yellow'>
+          <div className='rce-smsg-text'>{props.text}</div>
+        </div>    
+      }
+      {props.text !== 'closed_this_case' && props.text !== 'auto_closed_this_case' &&
+       props.text !== 'opened_a_case' && props.text !== 'reopened_this_case' &&
+       props.text !== 'connected_to_serve_you' &&
+        <div className='rce-smsg'>
+         <div className='rce-smsg-text'>{props.text}</div>
+        </div>
+       }
+       <div className='rce-smsg-time'>
+       </div>
     </div>
   )
 }
