2b72b25f3b14656242cd780b39db42a137c501bb
wamun <yves@debyoo.com>
2023-01-10 01:05:00 +0100

    Update SystemMessage.tsx

--- src/SystemMessage/SystemMessage.tsx
+++ src/SystemMessage/SystemMessage.tsx
@@ -8,31 +8,41 @@ import { ISystemMessageProps } from '../type'
 const SystemMessage: React.FC<ISystemMessageProps> = props => {
   return (
     <div className={classNames('rce-container-smsg', props.className)}>
-      {(props.text === 'closed_this_case' || props.text === 'auto_closed_this_case') &&
+      {(props.text.indexOf('closed') > -1 || props.text.indexOf('clôturé') > -1) &&
         <div className='rce-smsg-red'>
           <div className='rce-smsg-text'>{props.text}</div>
+          <div className='rce-smsg-time'>
+           {format(props.date)}
+         </div>
         </div>    
       }
-      {(props.text === 'opened_a_case' || props.text === 'reopened_this_case') &&
+      {(props.text.indexOf('opened') > -1 || props.text.indexOf('ouvert') > -1) &&
         <div className='rce-smsg-green'>
           <div className='rce-smsg-text'>{props.text}</div>
+          <div className='rce-smsg-time'>
+           {format(props.date)}
+         </div>
         </div>    
       }
-      {props.text === 'connected_to_serve_you' &&
+      {props.text.indexOf('connect') > -1 &&
         <div className='rce-smsg-yellow'>
           <div className='rce-smsg-text'>{props.text}</div>
+          <div className='rce-smsg-time'>
+           {format(props.date)}
+         </div>
         </div>    
       }
-      {props.text !== 'closed_this_case' && props.text !== 'auto_closed_this_case' &&
-       props.text !== 'opened_a_case' && props.text !== 'reopened_this_case' &&
-       props.text !== 'connected_to_serve_you' &&
+      {props.text.indexOf('closed') < 0 && props.text.indexOf('clôturé') < 0 &&
+       props.text.indexOf('opened') < 0 && props.text.indexOf('ouvert') < 0 &&
+       props.text.indexOf('connect') < 0 &&
         <div className='rce-smsg'>
          <div className='rce-smsg-text'>{props.text}</div>
+         <div className='rce-smsg-time'>
+           {format(props.date)}
+         </div>
         </div>
        }
-       <div className='rce-smsg-time'>
-           {format(props.date)}
-       </div>
+       
     </div>
   )
 }
