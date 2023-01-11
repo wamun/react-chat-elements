f9b59ff565e0656fea2f44dee4294ddb76758048
wamun <yves@debyoo.com>
2023-01-11 17:59:18 +0100

    Update SystemMessage.tsx

--- src/SystemMessage/SystemMessage.tsx
+++ src/SystemMessage/SystemMessage.tsx
@@ -6,6 +6,15 @@ import classNames from 'classnames'
 import { ISystemMessageProps } from '../type'
 
 const SystemMessage: React.FC<ISystemMessageProps> = props => {
+  let name, title, fbanswer, rating;
+  if (props.text.indexOf('case_closure_notes') > -1)
+  {
+      let s = JSON.parse(props.text);
+      name = s.name;
+      title = `${name} sent Closure Notes`;
+      fbanswer = s.fbanswer;
+      rating = s.rating;
+  }
   return (
     <div className={classNames('rce-container-smsg', props.className)}>
       {(props.text.indexOf('closed') > -1 || props.text.indexOf('clôturé') > -1) &&
@@ -32,9 +41,25 @@ const SystemMessage: React.FC<ISystemMessageProps> = props => {
          </div>
         </div>    
       }
+      {props.text.indexOf('case_closure_notes') > -1 &&
+        <div className='rce-smsg'>
+            <div className='rce-smsg-text'>{title}</div>
+            
+            <div className='rce-smsg-text mrgtop-10'>{`Please let us know if we helped resolve your issue:`}</div>
+                  
+           {fbanswer === 'Yes' && <div className='rce-smsg-text-green mrgtop-5'>{`YES`}</div>}
+           {fbanswer === 'No' && <div className='rce-smsg-text-red mrgtop-5'>{`NO`}</div>}
+           {fbanswer !== 'No' && fbanswer !== 'Yes' && <div className='rce-smsg-text mrgtop-5'>{`-`}</div>}
+                  
+            <div className='rce-smsg-text mrgtop-10'>{`Rate this correspondence from 1 to 5 stars:`}</div>
+                  
+            {rating < 1 && <div className='rce-smsg-text mrgtop-5'>{`-`}</div>}
+            {rating > 0 && <div className='rce-smsg-text mrgtop-5'>{rating}</div>}
+        </div>    
+      }
       {props.text.indexOf('closed') < 0 && props.text.indexOf('clôturé') < 0 &&
        props.text.indexOf('opened') < 0 && props.text.indexOf('ouvert') < 0 &&
-       props.text.indexOf('connect') < 0 &&
+       props.text.indexOf('connect') < 0 && props.text.indexOf('case_closure_notes') < 0 &&
         <div className='rce-smsg'>
          <div className='rce-smsg-text'>{props.text}</div>
          <div className='rce-smsg-time'>
